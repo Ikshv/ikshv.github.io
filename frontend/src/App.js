3254e3aafddb5757cf3ab9c90efaef00f2afc0a2
Ikshv <ikshvartsman@gmail.com>
2025-03-13 00:04:51 -0700

    revamp homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,23 +7,25 @@ import ToggleMessage from './ToggleMessage';
 import './App.css';
 
 import Home from './pages/Home';
+import Projects from './pages/Projects'
 import Header from './components/Header';
+import Footer from './components/Footer'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header title="Personal Website" subtitle="Welcome to my site!"/>
-      {/* <Router> */}
-        <Routes>
-          <Route path="/" element={<Hello name="world" />} />
-          <Route path="/counter" element={<Counter />} />
-          <Route path="/nameinput" element={<NameInput />} />
-          <Route path="/datafetcher" element={<DataFetcher />} />
-          <Route path="/togglemessage" element={<ToggleMessage />} />
-        </Routes>
-      {/* </Router> */}
-      {/* <Home /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hello" element={<Hello name="world" />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/nameinput" element={<NameInput />} />
+        <Route path="/datafetcher" element={<DataFetcher />} />
+        <Route path="/togglemessage" element={<ToggleMessage />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Hero from '../components/Hero';
 
 function Home() {
     return (
         <div>
-            <h1>Personal Website</h1>
-            <p>This is the home page.</p>
+            {/* <h1>Personal Website</h1>
+            <p>This is the home page.</p> */}
+            <Hero />
+            {/* add additional sections below */}
         </div>
     );
 }
