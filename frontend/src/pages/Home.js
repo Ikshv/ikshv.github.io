73f3eae62d99cf2e89bc6d4ef57d0c38888551d3
IKshv <ikshvartsman@gmail.com>
2025-03-13 14:38:23 -0700

    lets go full stack

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Hero from '../components/Hero';
-
+import Api from '../components/ApiReceiver'
 function Home() {
     return (
         <div>
             {/* <h1>Personal Website</h1>
             <p>This is the home page.</p> */}
+            <Api />
             <Hero />
             {/* add additional sections below */}
         </div>
