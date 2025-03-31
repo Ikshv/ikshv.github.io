1005def4f5a0f8d6d3eb8523aa96285d5b4e1e46
Ikshv <ikshvartsman@gmail.com>
2025-03-31 01:15:29 -0700

    fix

--- frontend/scripts/generate_index.py
+++ frontend/scripts/generate_index.py
@@ -1,23 +1,31 @@
 import requests
 import json
+import base64
 
-username = "ikshv"
-url = f"https://api.github.com/users/{username}/repos"
+
+def getRepos():
+    username = "ikshv"
+    url = f"https://api.github.com/users/{username}/repos"
+    response = requests.get(url)
+
+    repos = response.json()
+
+    for repo in repos:
+        print(
+            {
+                "title": repo["name"],
+                "url": repo["html_url"],
+                # "description": repo["description"],
+                # "created_at": repo["created_at"],
+                # "updated_at": repo["updated_at"]
+            }
+        )
+
+    return repos
+
+url = "https://api.github.com/repos/ikshv/ikshv.github.io/contents/projects.json"
 response = requests.get(url)
-
-repos = response.json()
-
-for repo in repos:
-    print(
-        {
-            "title": repo["name"],
-            "url": repo["html_url"],
-            # "description": repo["description"],
-            # "created_at": repo["created_at"],
-            # "updated_at": repo["updated_at"]
-        }
-    )
-
-    
-
-
+content = response.json()["content"]
+decoded = base64.b64decode(content).decode("utf-8")
+data = json.loads(decoded)
+print(data)
